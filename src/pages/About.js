0309b3a2cbdacb4ddd92e143074a32f33cab05e6
zelkovaria <hyein396@gmail.com>
2024-01-02 02:46:03 +0900

    13.4.2 쿼리스트링 사용

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,27 @@
+import { useSearchParams } from "react-router-dom";
+
 const About = () => {
+  const [searchParams, setSearchParmas] = useSearchParams();
+  const detail = searchParams.get("detail");
+  const mode = searchParams.get("mode");
+
+  const onToggleDetail = () => {
+    setSearchParmas({ mode, detail: detail === "true" ? false : true });
+  };
+
+  const onIncreaseMode = () => {
+    const nextMode = mode === null ? 1 : parseInt(mode) + 1;
+    setSearchParmas({ mode: nextMode, detail });
+  };
+
   return (
     <div>
       <h1>소개</h1>
       <p>리액트 라우터를 사용해 보는 프로젝트입니다.</p>
+      <p>detail: {detail}</p>
+      <p>mode: {mode}</p>
+      <button onClick={onToggleDetail}>Toggle detail</button>
+      <button onClick={onIncreaseMode}>mode + 1</button>
     </div>
   );
 };
