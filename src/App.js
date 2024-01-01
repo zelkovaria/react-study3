8645a909729cdf63c254d163d4a55b9ffca44a27
zelkovaria <hyein396@gmail.com>
2024-01-02 02:25:09 +0900

    13.4.1 URL 파라미터 사용

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/profiles/:username" element={<Profile />} />
     </Routes>
   );
 };
