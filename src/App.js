22508a25c1c9c982a1fcadd2cac1a3635624b187
zelkovaria <hyein396@gmail.com>
2024-01-02 20:46:11 +0900

    13.5 중첩된 라우트 다루기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import Article from "./pages/Article";
+import Articles from "./pages/Articles";
 
 const App = () => {
   return (
@@ -11,6 +12,9 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/profiles/:username" element={<Profile />} />
+      <Route path="/articels" element={<Articles />}>
+        <Route path="/articels/:id" element={<Article />} />
+      </Route>
     </Routes>
   );
 };
